6a76846e6d372dce670d6360f8523ff2b9d644c8
rmidou <139585550+rmidou@users.noreply.github.com>
2023-07-27 07:33:01 +0200

    Update ft_find_next_prime.c

--- piscine/c05git/ex07/ft_find_next_prime.c
+++ piscine/c05git/ex07/ft_find_next_prime.c
@@ -28,7 +28,10 @@ int	ft_is_prime(int nb)
 
 int	ft_find_next_prime(int nb)
 {
-	while (ft_is_prime(nb) != 1)
-		nb++ ;
-	return (nb);
+	while nb <= 2147483647
+	{
+		while (ft_is_prime(nb) != 1)
+			nb++ ;
+	}
+		return (nb);
 }
